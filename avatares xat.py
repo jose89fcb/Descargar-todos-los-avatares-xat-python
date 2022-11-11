73cb5a3a174ea843fb33bfb857e90457032fb628
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-11-11 21:13:06 +0100

    Add files via upload

---
+++ avatares xat.py
@@ -0,0 +1,29 @@
+import urllib.request
+import os 
+import pathlib
+
+
+
+
+
+
+
+                                                                                                            
+
+
+pathlib.Path(f"Avatares xat").mkdir(parents=True, exist_ok=True)
+os.chdir(os.path.join(os.getcwd(),f"avatares xat")) 
+
+
+totalAvatares=0
+for i in range(1,1759):
+    totalAvatares=totalAvatares+1
+   
+    DescargarAvatar= open(f'{i}.png','wb')
+
+    print(f"Descargando-> https://xat.com/web_gear/chat/av/{i}.png")
+    
+    DescargarAvatar.write(urllib.request.urlopen(f'https://xat.com/web_gear/chat/av/{i}.png').read())
+    
+    DescargarAvatar.close()
+print(f"Se han descargado un total de {totalAvatares} sonidos.")
